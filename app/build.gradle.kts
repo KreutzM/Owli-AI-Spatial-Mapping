654fc4f5f21c7bc859f8a89244c96a28546aec43
Michak <121102425+KreutzM@users.noreply.github.com>
2026-08-05 15:07:22 +0200

    build: align Android 36 bootstrap compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,11 @@ kotlin {
 
 android {
     namespace = "com.owlitech.spatial"
-    compileSdk = 36
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
 
     defaultConfig {
         applicationId = "com.owlitech.spatial"
